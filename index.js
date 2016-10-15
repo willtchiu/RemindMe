e7a7a0d29a5e80001f28863266241a930505b26e
willtchiu <wtchiu@ucsd.edu>
2016-10-14 22:30:30 -0700

    added echo behaviour to bot

--- index.js
+++ index.js
@@ -19,3 +19,36 @@ app.get('/webhook', function (req, res) {
         res.send('Invalid verify token');
     }
 });
+
+// handler for reveiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+        //if there's a message respond to the message
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
+    }
+    res.sendStatus(200);
+});   
+
+//general function for sending messages
+function sendMessage(recipientId, message) {
+    request({
+        //only messages back to me in testing
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function (error) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
